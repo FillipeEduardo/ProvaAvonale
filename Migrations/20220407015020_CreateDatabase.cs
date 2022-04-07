01d8ef5948a837a685f28b688d8b2b4fcde40614
lipaoos <fillipe_oliveira@outlook.com.br>
2022-04-06 22:52:39 -0300

    gerado os migrations

---
+++ Migrations/20220407015020_CreateDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProvaAvonale.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estoques",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NomeProduto = table.Column<string>(type: "TEXT", nullable: false),
+                    ValorUnitario = table.Column<double>(type: "REAL", nullable: false),
+                    QuantidadeEstoque = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataUltimaVenda = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ValorUltimaVenda = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estoques", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estoques");
+        }
+    }
+}
